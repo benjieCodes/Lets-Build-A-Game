5cdded9043c9d14d1d8e3fac46ee5c058c0a981f
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-10 09:08:06 -0400

    added tests for the you and game classes

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 import chai from 'chai';
 
 // Import Any Files to Test
-import '../src/js/main';
+// import '../src/js/main';
 import { You } from '../src/js/classes/you.js'
 import { Game } from '../src/js/classes/game.js'
 
@@ -11,20 +11,16 @@ const expect = chai.expect;
 const should = chai.should();
 const assert = chai.assert;
 
-describe('Something We Want To Test', function () {
-
-  describe('Testing the Creation of Something', function () {
+describe('Testing the Creation of Something', function () {
 
     it('should be in an instance of a player when created', function () {
       let player = new You;
       expect(player).to.be.an.instanceof(You);
     });
 
-    it('should be an instance of a game when created'), function () {
+    it('should be an instance of a game when created', function () {
       let game = new Game;
-      expect(game).to.be.an.instanceof(Gane);
+      expect(game).to.be.an.instanceof(Game);
     });
-  });
-
 
 });
