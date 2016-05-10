e8f40f59d2a4caa39d0cb57517e9e629fe820211
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-09 22:49:48 -0400

    could not npm run test

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ import chai from 'chai';
 
 // Import Any Files to Test
 import '../src/js/main';
+import { You } from '../src/js/classes/you.js'
+import { Game } from '../src/js/classes/game.js'
 
 // Set Chai Constants
 const expect = chai.expect;
@@ -13,16 +15,15 @@ describe('Something We Want To Test', function () {
 
   describe('Testing the Creation of Something', function () {
 
-    let game;
-    beforeEach( function (){
-      game = new You();
+    it('should be in an instance of a player when created', function () {
+      let player = new You;
+      expect(player).to.be.an.instanceof(You);
     });
 
-    it('should be in an instance of a player', function () {
-      let x = new You();
-      expect(game).to.be.an.instanceof(You);
+    it('should be an instance of a game when created'), function () {
+      let game = new Game;
+      expect(game).to.be.an.instanceof(Gane);
     });
-
   });
 
 
